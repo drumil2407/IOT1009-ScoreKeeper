0174f2058a7c0165f842581e2554bed61b603709
drumil2407 <78135386+drumil2407@users.noreply.github.com>
2022-02-26 21:44:16 -0500

    Lab-3
    - Increase and decrease score when click on button on team 1

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView item;
+    Button increase, decrease;
+    int i = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find a text view when button click
+        item = (TextView) findViewById(R.id.team1_score);
+        increase = (Button) findViewById(R.id.team2_button2);
+        decrease = (Button) findViewById(R.id.team2_button3);
+
+
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i++;
+                item.setText(String.valueOf(i));
+            }
+        });
+
+        decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //i will stop at 1 then it will not go negative
+                if (i != 0)
+                    i--;
+                item.setText(String.valueOf(i));
+            }
+        });
+
+
+    }
+
+    public void radioButtonChecked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radioButton:
+                if (checked)
+                    break;
+            case R.id.radioButton2:
+                if (checked)
+                    break;
+        }
+
     }
 }
